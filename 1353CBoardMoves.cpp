674f90b993355d5057054c2a31e71bb444635d47
Mitender Kumar <50289522+mitender@users.noreply.github.com>
2020-05-31 10:44:50 +0530

    Add files via upload

---
+++ 1353CBoardMoves.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<iomanip>
+#include<cmath>
+#include<math.h>
+#define ll long long
+#define PI 3.14159265358979323846264338327950288
+using namespace std;
+int main(void){
+ 
+ll t,n;
+cin>>t;
+while(t--){
+    cin>>n;
+    n=(n-1)/2;
+cout<<8*((n*(n+1)*(2*n+1))/6)<<endl;
+}
+    }
+
+
